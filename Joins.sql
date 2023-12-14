bd63ba6fcbd49e85f580e6316fe2705e95f3b44d
Kerry Metayer <KMetayer@appdev.yearup.org>
2023-12-14 12:02:08 -0500

    added joins

---
+++ Joins.sql
@@ -0,0 +1,5 @@
+use northwind;
+-- question 1
+-- select ProductID, ProductName, UnitPrice, CategoryName from products join categories  order by CategoryName, ProductName;
+-- question 2
+select productID, productName, UnitPrice, SupplierName from products j
